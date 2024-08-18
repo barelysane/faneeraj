600d4eb8c01677c925b6017913f55cf7e6c82191
barelysane <neerajmanoj2005@gmail.com>
2024-08-18 21:08:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,98 +1,71 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv("C:/Users/moham/Downloads/train_LZdllcl.csv")
-from sklearn import preprocessing
-from sklearn import preprocessing
-encoder = preprocessing.LabelEncoder()
+# Load and preprocess training data
+df_train = pd.read_csv("C:/Users/neera/Downloads/train_LZdllcl.csv")
 
-encoder.fit(df['department'])
-df['department'] = encoder.transform(df['department'])
+# Initialize label encoder
+encoder = LabelEncoder()
 
-encoder.fit(df['region'])
-df['region'] = encoder.transform(df['region'])
+# Fit and transform columns
+df_train['department'] = encoder.fit_transform(df_train['department'])
+df_train['region'] = encoder.fit_transform(df_train['region'])
+df_train['education'] = encoder.fit_transform(df_train['education'])
+df_train['gender'] = encoder.fit_transform(df_train['gender'])
+df_train['recruitment_channel'] = encoder.fit_transform(df_train['recruitment_channel'])
 
-encoder.fit(df['education'])
-df['education'] = encoder.transform(df['education'])
+print(df_train.head())
 
-encoder.fit(df['gender'])
-df['gender'] = encoder.transform(df['gender'])
+# Define features and target
+X = df_train.drop(columns='is_promoted')
+y = df_train['is_promoted']
 
-encoder.fit(df['recruitment_channel'])
-df['recruitment_channel'] = encoder.transform(df['recruitment_channel'])
-print(df.head())
-
-x = df.iloc[:,:-1]
-
-print(x.head())
-
-y = df.iloc[:,-1]
-
-
-print(y.head())
-
-cat = ['region','gender']
-xf = pd.DataFrame(x.drop(columns=cat))
-
-X_train, X_test, y_train, y_test = train_test_split(xf, y, test_size=0.2, random_state=42)
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print(X_train.head())
 
+# Initialize and train XGBoost model
 model = xgb.XGBClassifier()
-
 model.fit(X_train, y_train)
 
-pred=model.predict(X_test)
+# Predictions and evaluation
+pred = model.predict(X_test)
 print(pred)
-cm = confusion_matrix(y_test,pred)
+
+cm = confusion_matrix(y_test, pred)
 print(cm)
-print(accuracy_score(y_test,pred))
+print(accuracy_score(y_test, pred))
 
+# Load and preprocess test data
+df_test = pd.read_csv("C:/Users/neera/Downloads/test_2umaH9m.csv")
 
+# Use the same encoder fitted on training data to avoid issues with unseen labels
+df_test['department'] = encoder.transform(df_test['department'])
+df_test['region'] = encoder.transform(df_test['region'])
+df_test['education'] = encoder.transform(df_test['education'])
+df_test['gender'] = encoder.transform(df_test['gender'])
+df_test['recruitment_channel'] = encoder.transform(df_test['recruitment_channel'])
 
-df = pd.read_csv("C:/Users/moham/Downloads/test_2umaH9m.csv")
+print(df_test.head())
 
-encoder = preprocessing.LabelEncoder()
+# Prepare features for prediction
+X_test_final = df_test.drop(columns=['employee_id'])  # Drop non-feature columns
 
-encoder.fit(df['department'])
-df['department'] = encoder.transform(df['department'])
+print("Test Data: ", X_test_final)
 
-encoder.fit(df['region'])
-df['region'] = encoder.transform(df['region'])
+# Make predictions on the test set
+pred_final = model.predict(X_test_final)
 
-encoder.fit(df['education'])
-df['education'] = encoder.transform(df['education'])
+# Prepare submission DataFrame
+submission = pd.DataFrame({
+    'employee_id': df_test['employee_id'],
+    'is_promoted': pred_final
+})
 
-encoder.fit(df['gender'])
-df['gender'] = encoder.transform(df['gender'])
-
-encoder.fit(df['recruitment_channel'])
-df['recruitment_channel'] = encoder.transform(df['recruitment_channel'])
-print(df.head())
-
-
-
-cat = ['region','gender']
-xf = pd.DataFrame(df.drop(columns=cat))
-print("Test: ",xf)
-
-pred=model.predict(xf)
-import numpy as np
-import pandas as pd
-prediction = pd.DataFrame(pred, columns=['is_promoted']).to_csv('prediction.csv')
-
-df1 = pd.read_csv("prediction.csv")
-df2 = df1[['predictions']].copy()
-print(df2.head())
-
-df3 = df[['employee_id']].copy()
-print(df3.head())
-
-both = df3.join(df2)
-print(both)
-
-both.to_csv("predictions3.csv", index=False)
+# Save to CSV
+submission.to_csv('predictions3.csv', index=False)
